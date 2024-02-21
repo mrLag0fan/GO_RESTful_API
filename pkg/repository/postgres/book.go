b008bcbd99ebb8441b73eeee7c98a375daa10720
Oleg Shalapsky <75427191+mrLag0fan@users.noreply.github.com>
2024-02-21 14:32:03 +0200

    Refactor 2

--- pkg/repository/postgres/book.go
+++ pkg/repository/postgres/book.go
@@ -1,8 +1,9 @@
-package repository
+package postgres
 
 import (
 	"GO_RESTful_API/pkg/database"
-	e "GO_RESTful_API/pkg/entity"
+	e "GO_RESTful_API/pkg/entities"
+	e2 "GO_RESTful_API/pkg/entities/impl/book"
 	"GO_RESTful_API/pkg/logger"
 	"GO_RESTful_API/pkg/my_uuid"
 	"database/sql"
@@ -25,7 +26,7 @@ func NewBookRepository(generator my_uuid.UuidGenerator) *BookRepository {
 
 func (repo *BookRepository) Create(entity e.Entity) bool {
 	logger.Log("trace", "Book insertion into database begun....")
-	book := e.EntityToBook(entity)
+	book := e2.EntityToBook(entity)
 	_, err := repo.DB.Exec(`INSERT INTO "book" VALUES ($1, $2, $3, $4, $5)`,
 		repo.uuidGenerator.GenerateUUID(),
 		book.Title,
@@ -53,7 +54,7 @@ func (repo *BookRepository) Delete(ID string) bool {
 
 func (repo *BookRepository) Update(ID string, entity e.Entity) bool {
 	logger.Log("trace", "Book updating from database begun....")
-	book := e.EntityToBook(entity)
+	book := e2.EntityToBook(entity)
 	_, err := repo.DB.Exec(`UPDATE "book" SET 
                     title=$1, 
                     numberofpages=$2,
@@ -76,7 +77,7 @@ func (repo *BookRepository) Update(ID string, entity e.Entity) bool {
 func (repo *BookRepository) GetByID(ID string) e.Entity {
 	logger.Log("trace", "Book receiving from database begun....")
 	row := repo.DB.QueryRow("SELECT * FROM book WHERE id = $1", ID)
-	var book e.Book
+	var book e2.Book
 	err := row.Scan(&book.ID, &book.Title, &book.NumberOfPages, &book.Description, &book.AuthorID)
 	if err != nil {
 		log.Printf("%s - [ERROR] %s \n", time.Now(), err.Error())
@@ -98,7 +99,7 @@ func (repo *BookRepository) GetAll() []e.Entity {
 
 	var res []e.Entity
 	for rows.Next() {
-		var book e.Book
+		var book e2.Book
 		err := rows.Scan(
 			&book.ID,
 			&book.Title,
@@ -112,6 +113,18 @@ func (repo *BookRepository) GetAll() []e.Entity {
 	}
 	logger.Log("trace", "Receiving all books from database finished.")
 	return res
+}
+
+func (repo *BookRepository) Exist(ID string) bool {
+	logger.Log("trace", "Checking whether the book exists....")
+	var exists bool
+	err := repo.DB.QueryRow("SELECT (exists(SELECT 1 FROM book WHERE id = $1))", ID).Scan(&exists)
+	if err != nil {
+		logger.Log("error", err.Error())
+		return false
+	}
+	logger.Log("trace", "Checking whether the book exists finished.")
+	return exists
 }
 
 func (repo *BookRepository) Clear() bool {
